356d39b2b84e3514b94a475497aa65bea3494fe7
Kingster <atarneo2015@gmail.com>
2021-12-16 22:49:54 -0800

    trigger build

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const CompressionWebpackPlugin = require('compression-webpack-plugin')
 
 const productionGzipExtensions = ['js', 'css']
 const isProd = process.env.NODE_ENV === 'production'
-
+// triger build
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ? '/' : './',
     productionSourceMap: false,
